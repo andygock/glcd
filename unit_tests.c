6266abd2f1df133f6e775c5925d066cd88690266
Andy Gock <andy@gock.net>
2012-06-17 01:22:29 +1000

    Added Modified BSD License details to files.

--- unit_tests.c
+++ unit_tests.c
@@ -3,14 +3,43 @@
  *
  */
 
+/*
+	Copyright (c) 2012, Andy Gock
+
+	All rights reserved.
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+		* Redistributions of source code must retain the above copyright
+		  notice, this list of conditions and the following disclaimer.
+		* Redistributions in binary form must reproduce the above copyright
+		  notice, this list of conditions and the following disclaimer in the
+		  documentation and/or other materials provided with the distribution.
+		* Neither the name of the copyright holders nor the
+		  names of its contributors may be used to endorse or promote products
+		  derived from this software without specific prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+	DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+	DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #include "glcd.h"
 #include "unit_tests.h"
 
 /** Fonts */
 #include "library/glcd/fonts/font5x7.h"
 //#include "library/glcd/fonts/Bebas_Neue20x36_Bold_Numbers.h"
+#include "library/glcd/fonts/Liberation_Sans15x21_Numbers.h"
 //#include "library/glcd/fonts/Liberation_Sans27x36_Numbers.h"
-#include "library/glcd/fonts/HelveticaNeueLT_Com_57_Cn23x35_Numbers.h"
+//#include "library/glcd/fonts/HelveticaNeueLT_Com_57_Cn23x35_Numbers.h"
 
 void glcd_test_circles(void)
 {
@@ -39,11 +68,11 @@ void glcd_test_counter_and_graph(void)
 		//glcd_tiny_set_font(Font5x7,5,7,32,127);
 		//glcd_draw_string_xy(0,40,(char *)utoa(count,string,10));
 
-		//glcd_set_font(Liberation_Sans15x21_Numbers,15,21,46,57);
+		glcd_set_font(Liberation_Sans15x21_Numbers,15,21,46,57);
 		//glcd_set_font(Liberation_Sans27x36_Numbers,27,36,46,57);
 		//glcd_set_font(Bebas_Neue20x36_Bold_Numbers,20,36,46,57);
 		//glcd_set_font(Bebas_Neue18x36_Numbers,18,36,46,57);
-		glcd_set_font(HelveticaNeueLT_Com_57_Cn23x35_Numbers,23,35,46,57);
+		//glcd_set_font(HelveticaNeueLT_Com_57_Cn23x35_Numbers,23,35,46,57);
 
 		sprintf(string,"%d",count);
 		glcd_draw_string_xy(0,0,string);

8ef68f216a5ca77b1577a1df2a0fb592100a063e
Andy Gock <andy@gock.net>
2012-06-17 23:34:56 +1000

    Improved unit tests Add .gitignore

--- unit_tests.c
+++ unit_tests.c
@@ -31,29 +31,50 @@
 	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "glcd.h"
 #include "unit_tests.h"
 
 /** Fonts */
 #include "library/glcd/fonts/font5x7.h"
-//#include "library/glcd/fonts/Bebas_Neue20x36_Bold_Numbers.h"
+//#include "library/glcd/fonts/Liberation_Sans11x14_Numbers.h"
 #include "library/glcd/fonts/Liberation_Sans15x21_Numbers.h"
+//#include "library/glcd/fonts/Liberation_Sans17x17_Alpha.h"
 //#include "library/glcd/fonts/Liberation_Sans27x36_Numbers.h"
+//#include "library/glcd/fonts/Bebas_Neue20x36_Bold_Numbers.h"
+
+// Commercial fonts not included with distribution
 //#include "library/glcd/fonts/HelveticaNeueLT_Com_57_Cn23x35_Numbers.h"
+//#include "library/glcd/fonts/HelveticaNeueLT_Com_95_Blk18x19.h"
 
 void glcd_test_circles(void)
 {
+	uint8_t x,y,radius;
+
 	while (1) {
 		glcd_clear();
-		for (uint8_t i=0; i<=20; i++) {
-			glcd_fill_circle(20,20,i,BLACK);
+
+		// generate random(ish) position on display
+		x = rand() % GLCD_LCD_WIDTH;
+		y = rand() % GLCD_LCD_HEIGHT;
+		radius = rand() % 50;
+
+		//x = 70; y=25; radius=50; // for debugging
+
+		// fill circle with black
+		for (uint8_t i=0; i<=radius; i++) {
+			glcd_fill_circle(x,y,i,BLACK);
 			glcd_write();
 			_delay_ms(2);
 		}
-		for (uint8_t i=0; i<=20; i++) {
-			glcd_fill_circle(20,20,i,WHITE);
+
+		// fill the same circle above but with white
+		for (uint8_t i=0; i<=radius; i++) {
+			glcd_fill_circle(x,y,i,WHITE);
 			glcd_write();
-			_delay_ms(2);
+			_delay_ms(1);
 		}
 	}
 }
@@ -72,7 +93,7 @@ void glcd_test_counter_and_graph(void)
 		//glcd_set_font(Liberation_Sans27x36_Numbers,27,36,46,57);
 		//glcd_set_font(Bebas_Neue20x36_Bold_Numbers,20,36,46,57);
 		//glcd_set_font(Bebas_Neue18x36_Numbers,18,36,46,57);
-		//glcd_set_font(HelveticaNeueLT_Com_57_Cn23x35_Numbers,23,35,46,57);
+		//glcd_set_font(HelveticaNeueLT_Com_57_Cn23x35_Numbers,23,35,46,57); // commercial font - not for public distribution
 
 		sprintf(string,"%d",count);
 		glcd_draw_string_xy(0,0,string);
@@ -86,27 +107,35 @@ void glcd_test_counter_and_graph(void)
 
 }
 
-void glcd_test_tiny_up_down(void)
+void glcd_test_text_up_down(void)
 {
-	uint8_t x,y;
+	// moves some text up and down the display
 
-	glcd_tiny_set_font(Font5x7,5,7,32,127);
+	uint8_t y;
+
+	//glcd_set_font(Liberation_Sans11x14_Numbers,11,14,46,57);
+	glcd_set_font(Liberation_Sans15x21_Numbers,15,21,46,57);
+	//glcd_set_font(Liberation_Sans17x17_Alpha,17,17,46,90);
+
+	uint8_t max_y = GLCD_LCD_HEIGHT - font_current.height - 2; // max y start position for draw_string
 
 	while(1) {
-
-		for (y=0; y<43; y++) {
+		// move top to bottom
+		for (y=0; y<max_y; y++) {
 			glcd_clear_buffer();
-			glcd_draw_string_xy(0,y,"DOWN");
+			glcd_draw_string_xy(0,y,"123");
 			glcd_write();
-			_delay_ms(100);
+			_delay_ms(80);
 		}
 
-		for (y=0; y<43; y++) {
+		// move bottom to top
+		for (y=(max_y); y>0; y--) {
 			glcd_clear_buffer();
-			glcd_draw_string_xy(0,42-y,"UP");
+			glcd_draw_string_xy(0,y,"456");
 			glcd_write();
-			_delay_ms(100);
+			_delay_ms(80);
 		}
+
 
 	}
 
--- unit_tests.h
+++ unit_tests.h
@@ -33,14 +33,12 @@
 #ifndef _UNIT_TESTS_H
 #define _UNIT_TESTS_H
 
-#if defined(GLCD_DEVICE_LPC111X)
-	extern void delay_ms(uint32_t ms);
-	#define PROGMEM
-	#define _delay_ms(t) delay_ms(t)
-#endif
-
+/** Make random "exploding circles" */
 void glcd_test_circles(void);
+
 void glcd_test_counter_and_graph(void);
-void glcd_test_tiny_up_down(void);
+
+/** Scroll some text up and down the screen */
+void glcd_test_text_up_down(void);
 
 #endif
